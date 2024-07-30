ee9e56acbace87f110c048b8932ceecb515a6592
Jonas Otávio B. de Oliveira <120534373+Jonas01101111@users.noreply.github.com>
2024-07-30 14:07:33 -0300

    Add files via upload

---
+++ carros/incluir.php
@@ -0,0 +1,24 @@
+<h3>PHP & MySQL</h3>
+<body>
+<h3>Novo Produto</h3>
+<?php
+    include_once('conexao.php');
+    $codigo = $_POST['codigo'];
+    $produto= $_POST['produto'];
+    $descricao = $_POST['descricao'];
+    $imagem = $_POST['imagem'];
+    $data = $_POST['data'];
+    $valor = $_POST['valor'];
+    $sqlinsert = "insert into tabelaimg (codigo, produto, descricao, data, imagem, valor)
+                values ('$codigo', '$produto', '$descricao', '$data', '$imagem', $valor)";
+    $resultado = mysqli_query($conexao, $sqlinsert);
+    if (!$resultado){
+        die('Query Invalida: ' .mysqli_errno($conexao));
+    }else {
+        echo "Registro Cadastro com Sucesso";
+    }
+    mysqli_close($conexao)
+?>
+<br><br>
+<input type="button1" id="arroz" onclick="window.location='formprodutos.html';" value="Voltar">
+</body>
---
+++ carros/tabelaimg.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 30-Abr-2024 às 17:19
+-- Versão do servidor: 8.0.27
+-- versão do PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `banco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tabelaimg`
+--
+
+DROP TABLE IF EXISTS `tabelaimg`;
+CREATE TABLE IF NOT EXISTS `tabelaimg` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(14) NOT NULL,
+  `produto` varchar(80) NOT NULL,
+  `descricao` varchar(250) NOT NULL,
+  `data` date NOT NULL,
+  `imagem` varchar(80) NOT NULL,
+  `valor` double(15,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
